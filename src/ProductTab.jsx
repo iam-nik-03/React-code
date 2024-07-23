5b9b1cc892c8987c940b06d4f10d98d41951b384
Nik <nikdc03x@gmail.com>
2024-07-23 22:03:40 +0530

    rendering array via map method

--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -1,10 +1,10 @@
 import Product from "./product.jsx";
 function ProductTab() {
     let option = ["amoled screen", "WIFI-6E"];
-    // let option2 = { a: "pizzza", b: "Wanky wanky" };
+   
     return (
         <>
-         <Product Title="Phone" price={ 10000}  />
+
          <Product Title="Macbook" price={130000} features={option} />
         
             
--- src/product.jsx
+++ src/product.jsx
@@ -1,11 +1,15 @@
 import "./product.css";
-function Product({ Title, price ,features}) {
+function Product({ Title, price, features }) {
+
+    
     return (
         <div className="product">
         <h3>{Title}</h3>
             <h5>price: {price} </h5>
-            <h4>{features}</h4>
-           
+            
+            {/* <p>{list}</p>   */}   
+            {/* or */}
+            <p>{features.map((el) => <li>{el}</li> )}</p>
         
 
         </div>
