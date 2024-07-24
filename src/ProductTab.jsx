dbc767e69f7f1ae599d16addd5cc9cf42977e711
Nik <nikdc03x@gmail.com>
2024-07-24 12:40:34 +0530

    Conditionals etc

--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -5,7 +5,8 @@ function ProductTab() {
     return (
         <>
 
-         <Product Title="Macbook" price={130000} features={option} />
+         <Product Title="Macbook ProBook" price={130000} features={option} />
+         <Product Title="Macbook Air M3" price={60000} features={option} />
         
             
         </>
