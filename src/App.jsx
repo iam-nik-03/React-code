85df2300bb71d4d489cef1c07afd3a2780b6c0e5
Nik <nikdc03x@gmail.com>
2024-07-24 13:03:53 +0530

    Activity 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
 import Title from "./Title.jsx";
 import ProductTab from "./ProductTab.jsx";
+import MessageBox from "./Activity/MessageBox.jsx";
 
 function App() {
-  return <ProductTab />;
-    
+  return(
+    <>
+      <MessageBox userName="Nik" textColor="yellow"/>
+      <MessageBox userName="Om" textColor="blue"/>
+       <ProductTab />
+
+    </>
+  );
+  
 
 }
 
