f3484f87e79f003a38194f1dd6ea00a485a433ab
Nik <nikdc03x@gmail.com>
2024-07-27 23:14:51 +0530

    init clcok events

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import "./App.css";
-import Title from "./Title.jsx";
-import ProductTab from "./product/ProductTab.jsx";
-import MessageBox from "./Activity/MessageBox.jsx";
+
 
 function App() {
   return(
     <>
-      <MessageBox userName="Nik" textColor="yellow"/>
-      <MessageBox userName="Om" textColor="blue"/>
-       <ProductTab />
+
 
     </>
   );
-  
-
 }
 
 export default App;
